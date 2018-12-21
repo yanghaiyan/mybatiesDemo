5c6458e78be421973bf09c9f6b00e9912721a2a9
yanghaiyan <yanghaiy@koal.com>
2018-12-21 15:54:10 +0800

    add:增加https监听

--- src/main/java/com/mybatis/demo/DemoApplication.java
+++ src/main/java/com/mybatis/demo/DemoApplication.java
@@ -1,16 +1,58 @@
 package com.mybatis.demo;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan(basePackages = {"com.mybatis.demo.dao"})
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Value("${http.port}")
+  Integer httpPort;
+
+  @Value("${server.port}")
+  Integer httpsPort;
+
+  @Bean
+  public TomcatServletWebServerFactory servletContainer() {
+    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+      @Override
+      protected void postProcessContext(Context context) {
+        SecurityConstraint constraint = new SecurityConstraint();
+        constraint.setUserConstraint("CONFIDENTIAL");
+        SecurityCollection collection = new SecurityCollection();
+        collection.addPattern("/*");
+        constraint.addCollection(collection);
+        context.addConstraint(constraint);
+      }
+    };
+    tomcat.addAdditionalTomcatConnectors(httpConnector());
+    return tomcat;
+  }
+
+  @Bean
+  public Connector httpConnector() {
+    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    connector.setScheme("http");
+    //Connector监听的http的端口号
+    connector.setPort(httpPort);
+    connector.setSecure(false);
+    //监听到http的端口号后转向到的https的端口号
+    connector.setRedirectPort(httpsPort);
+    return connector;
+  }
 }
